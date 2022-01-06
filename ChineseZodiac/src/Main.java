cfd9b4b2dac6aec5c9f1cf00d7542558eaa38345
Onur Gürkan <onurgurkaann@gmail.com>
2022-01-06 20:34:29 +0300

    chinese zodiac

---
+++ ChineseZodiac/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Chinese Zodiac
+		
+		int year, result;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Doðum yýlýnýzý girin : ");
+		year = scan.nextInt();
+		year = year % 12;
+		
+		switch(year) {
+		case 0:
+			System.out.println("Çin Zodyaðý Burcunuz : Maymun");
+			break;
+		case 1:
+			System.out.println("Çin Zodyaðý Burcunuz : Horoz");
+			break;
+		case 2:
+			System.out.println("Çin Zodyaðý Burcunuz : Köpek");
+			break;
+		case 3:
+			System.out.println("Çin Zodyaðý Burcunuz : Domuz");
+			break;
+		case 4:
+			System.out.println("Çin Zodyaðý Burcunuz : Fare");
+			break;
+		case 5:
+			System.out.println("Çin Zodyaðý Burcunuz : Öküz");
+			break;
+		case 6:
+			System.out.println("Çin Zodyaðý Burcunuz : Kaplan");
+			break;
+		case 7:
+			System.out.println("Çin Zodyaðý Burcunuz : Tavþan");
+			break;
+		case 8:
+			System.out.println("Çin Zodyaðý Burcunuz : Ejderha");
+			break;
+		case 9:
+			System.out.println("Çin Zodyaðý Burcunuz : Yýlan");
+			break;
+		case 10:
+			System.out.println("Çin Zodyaðý Burcunuz : At");
+			break;
+		case 11:
+			System.out.println("Çin Zodyaðý Burcunuz : Koyun");
+			break;
+		default:
+			System.out.println("Bilgiler yanlýþ!");
+			break;
+		}
+	}
+
+}
